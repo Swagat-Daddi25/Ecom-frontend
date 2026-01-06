fdf98d7bbc47cd44707516ae10fefa35f18cee0d
Swagat-Daddi25 <daddiswagat@gmail.com>
2026-01-06 15:29:03 +0530

    commit changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About us</h1>
+      <br />
+      <h2>
+        Our mission is to create a shopping experience that feels simple,
+        trustworthy, and enjoyable — from the moment you browse to the moment
+        your order arrives at your door.
+      </h2>
+      <br />
+      <p>
+        Welcome to My Bag — where quality meets convenience. We started My Bag
+        with one simple goal: to make it easier for people to find products they
+        love, without compromising on quality, price, or experience. What began
+        as [a small idea / a passion project / a solution to a common problem]
+        has grown into an ecommerce store trusted by customers who value
+        reliability and great service.
+      </p>
+    </div>
+  );
 }
 
 export default About;
