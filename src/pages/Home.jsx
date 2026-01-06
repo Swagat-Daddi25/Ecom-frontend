79acad99315d7d4e1070ac06305238de2f9d6e61
Swagat-Daddi25 <daddiswagat@gmail.com>
2026-01-06 15:51:06 +0530

    pre commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>MY BAG</h1>
+      <b>Carry Your Style. Carry Your World.</b>
+      <h4>Shop Smart. Carry Better.</h4>
+      <p>
+        At MY BAG, we create and curate bags that fit your lifestyle. Whether
+        you’re heading to work, traveling, or stepping out casually, our bags
+        are made to keep up with you.
+      </p>
+      <h2>Why MY BAG?</h2>
+      <li>
+        <b>Premium Quality Materials</b>
+        Built to last with attention to detail.
+      </li>
+      <li>
+        <b>Modern & Functional Designs</b>
+        Stylish looks with smart storage.
+      </li>
+      <li>
+        <b>Comfort Meets Durability</b>
+        Easy to carry, strong enough for daily use.
+      </li>
+      <li>
+        <b>Affordable Pricing</b>
+        Quality bags without the premium price tag
+      </li>
+
+      <h3>
+        <b>
+          Find the Bag That Carries You Further Upgrade your everyday carry with
+          MY BAG.
+        </b>
+      </h3>
+    </div>
+  );
 }
 
 export default Home;
